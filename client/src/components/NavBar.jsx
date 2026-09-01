070473eefa7dbd05c75b47ee8bb76ea0d0d49675
cainlucastapp <cainlucastapp@agentformula.com>
2026-09-01 12:19:24 -0700

    Style Nav Bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,27 +2,37 @@
 
 import { Link } from 'react-router-dom'
 import { useAuth } from '../context/auth-context'
+import logo from '../assets/images/menu-logo.png'
+import '../styles/nav.css'
 
 export function NavBar() {
   const { user, logout } = useAuth()
 
   return (
-    <nav>
-      {user ? (
-        <>
-          <Link to="/cases">Cases</Link>
-          <Link to="/items">Items</Link>
-          <span>{user.first_name}</span>
-          <button type="button" onClick={logout}>
-            Log out
-          </button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Log in</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
+    <nav className="navbar">
+      <div className="container navbar-inner">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="Just In Case" className="navbar-logo" />
+        </Link>
+
+        {user ? (
+          <div className="navbar-actions">
+            <Link to="/cases">Cases</Link>
+            <Link to="/items">Items</Link>
+            <span className="navbar-user">{user.first_name}</span>
+            <button type="button" className="btn btn-secondary" onClick={logout}>
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <div className="navbar-actions">
+            <Link to="/login">Log in</Link>
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   )
 }
