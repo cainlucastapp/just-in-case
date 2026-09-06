fc7c9d3e060e02d37b001ff21d2652b824ecc5e0
Cain Tapp <cainlucastapp@gmail.com>
2026-09-06 14:15:20 -0700

    Disable Debug Mode By Default

--- api/run.py
+++ api/run.py
@@ -1,4 +1,6 @@
 # run.py
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,4 +10,5 @@ from app.app import create_app  # noqa: E402
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=5555, debug=True, threaded=True)
+    debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    app.run(port=5555, debug=debug, threaded=True)
